e7153f36a59d08d1f1a93574c8c789db974bfa97
khwahishindoria <khwahishindoria@gmail.com>
2023-12-11 18:10:19 +0530

    Update Linked-List.c

--- Linked-List.c
+++ Linked-List.c
@@ -4,16 +4,18 @@
 struct node{
     int data;
     int next;
-};
-struct node *first=NULL;
+}*first=NULL;
+
 
 void createLL(int A[], int n){
     int i=0;
     struct node *t, *last;
     first=(struct node *)malloc(sizeof(struct node));
+    first->data=A[0];
+    first->next=NULL;
     last=first;
     
-    for(i=0; i<n; i++){
+    for(i=1; i<n; i++){
         t=(struct node *)malloc(sizeof(struct node));
         t->data=A[i];
         t->next=NULL;
@@ -23,8 +25,19 @@ void createLL(int A[], int n){
     }
 }
 
+void display(struct node *p){
+    while(p != NULL){
+        printf("%d", p->data);
+        p=p->next;
+    }
+}
+
 int main()
 {
     int A[]={5, 3, 8, 9};
+    createLL(A, 4);
+    display(first);
+    
+    return 0;
     
 }
